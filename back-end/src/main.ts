d8d80c99deef4d73b8f174874502926817f380aa
eric <eric.christensen94@gmail.com>
2020-08-29 17:55:18 +0200

    Setup production stuff

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -9,6 +9,7 @@ import { INITIAL_MIDDLEWARE } from './rest-services/middlewares/initial.middlewa
 import { ArticleController } from './rest-services/staff/media/article.controller';
 import { RadioController } from './rest-services/staff/radio.controller';
 import { EventsController } from './rest-services/staff/events.controller';
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 class MainServer extends Server {
@@ -19,11 +20,16 @@ class MainServer extends Server {
         this.backgroundTaskHandler = new BackgroundTaskHandler();
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(bodyParser.json());
+        this.app.use(express.static(__dirname + '/public'));
+        this.app.use(express.static(__dirname + '/resources'));
     }
 
     start (port: number): void {
         createConnection().then(() => {
             this.setupControllers();
+            this.app.use('*', (req, res) => {
+                res.sendFile(__dirname + '/public/index.html');
+            });
             this.backgroundTaskHandler.activate();
             this.app.listen(port, () => {
                 console.log(`Server started on port ${port}`);
