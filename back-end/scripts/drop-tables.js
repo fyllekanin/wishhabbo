77752e4a37a39e75d4ff37f3cba9e5c837905b11
eric <eric.christensen94@gmail.com>
2020-09-05 15:23:46 +0200

    Fix

--- back-end/scripts/drop-tables.js
+++ back-end/scripts/drop-tables.js
@@ -19,8 +19,7 @@ async function run() {
                         return;
                     }
                     const query = result.reduce((prev, curr, index) => {
-                        console.log(curr);
-                        const tableName = curr.table_name ? curr.table_name : curr;
+                        const tableName = curr.table_name ? curr.table_name : curr.TABLE_NAME;
                         return prev + (index > 0 ? `,${tableName}` : ` ${tableName}`);
                     }, 'DROP TABLE IF EXISTS');
                     connection.query(`${query};`, (err, result) => {
