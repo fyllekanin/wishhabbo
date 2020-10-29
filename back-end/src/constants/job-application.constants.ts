0fa96117e1ff134d217e43b6ba40abc0b6332f27
Eric <eric.christensen94@gmail.com>
2020-10-29 18:03:33 +0100

    Add job roles

---
+++ back-end/src/constants/job-application.constants.ts
@@ -0,0 +1,17 @@
+export class JobApplicationConstants {
+
+    static ROLES = [
+        {
+            label: 'Events Host',
+            id: 1
+        },
+        {
+            label: 'Radio DJ',
+            id: 2
+        },
+        {
+            label: 'Media Journalist',
+            id: 3
+        }
+    ];
+}
